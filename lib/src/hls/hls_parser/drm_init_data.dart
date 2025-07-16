358278f75133a4cc37070d07b66f04d4ccc08f76
andreacarmagnola <andreacarmagnola@fulcri.it>
2025-07-16 17:19:41 +0200

    fix hashValues

--- lib/src/hls/hls_parser/drm_init_data.dart
+++ lib/src/hls/hls_parser/drm_init_data.dart
@@ -1,5 +1,6 @@
 import 'package:collection/collection.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 
 import 'scheme_data.dart';
 
--- lib/src/hls/hls_parser/scheme_data.dart
+++ lib/src/hls/hls_parser/scheme_data.dart
@@ -1,6 +1,7 @@
 import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 
 class SchemeData {
   SchemeData({
